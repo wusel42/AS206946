5f6c22554aec866155fd27bd7f401c45b3491a4f
wusel42 <wusel+src@uu.org>
2016-10-31 15:19:10 +0100

    Refactored ...

--- create_matrix.sh
+++ create_matrix.sh
@@ -2,6 +2,9 @@
 #
 # Build for interactive use, i. e. set PATH accordingly if run via cron
 #
-# Read as206946-hosts.txt and generate a matrix of connections.
+# Read as206946-hosts.txt and generate a matrix of connections, then add as206946-links.txt
+# to generate as206946-tunnel.txt
 
-awk < as206946-hosts.txt >as206946-tunnel.txt '{host[++num]=$1;} END {for(i=1; i<=num; i++) { for(j=i; j<=num; j++) {if(host[i] != host[j]) printf("%s-%s l2tp\n", host[i], host[j]);}}}'
+awk < as206946-hosts.txt >/tmp/tunnel-$$.tmp '{host[++num]=$1;} END {for(i=1; i<=num; i++) { for(j=i; j<=num; j++) {if(host[i] != host[j]) printf("%s-%s l2tp\n", host[i], host[j]);}}}'
+cat /tmp/tunnel-$$.tmp as206946-links.txt >as206946-tunnel.txt
+rm /tmp/tunnel-$$.tmp
